e6366e856044b76a3d0a23266ef09b5d0666513a
Abraham Diaz <abdiaz2018@gmail.com>
2020-08-21 17:08:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for(var i = 0; i < musicians.length; i++) {
+    array.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return array;
+}
+function johnLennonFacts() {
+  var facts = ["While on tour, he enjoyed playing Monopoly", "Harmonica was the first instrument Lennon learned to play"];
+  var array = [];
+  var i = 0;
+  while (array.length < facts.length) {
+    array.push(facts[i] + "!!!");
+    i++;
+  }
+  return array;
+}
